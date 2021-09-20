d4dbb2e6a5356c2ec0a0b818c9739ac6d9b400d4
Healsu <timmie2500@gmail.com>
2021-09-20 12:09:01 +0200

    haha

--- src/rockPaperScissor.java
+++ src/rockPaperScissor.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+
 public class rockPaperScissor {
     public static void main(String[] args) {
 
+        int rounds = 3;
         boolean vsBot = false;
         boolean vsPlayer = false;
 
@@ -20,39 +22,118 @@ public class rockPaperScissor {
             System.out.println("I couldnt make out what you were saying, soo i will assume you want to play offline");
             vsBot = true;
         }
-        playGame(vsBot,vsPlayer);
+
+        if (vsBot == true) {
+            playBot(rounds);
+        } else if (vsPlayer == true)
+            playPlayer(rounds);
+
     }
-    public static void playGame (boolean vsBot, boolean vsPlayer){
-        Scanner scanner1 = new Scanner(System.in);
-        System.out.println("Choose a move between rock, paper or scissor: ");
-        String player1Move = scanner1.nextLine();
+
+    //Going against the bot
+    public static void playBot(int rounds) {
+        boolean playerWin = false;
+        boolean botWin = false;
+
+        for (int i = 0; i < rounds; i++) {
 
 
-        if (vsPlayer==true){
-            Scanner scanner2 = new Scanner(System.in);
-            System.out.println("Since we have a player two, please choose between rocks, paper or scissor");
-            String player2Move = scanner2.nextLine();
-        }
-        else if(vsBot ==true){
+            Scanner scanner1 = new Scanner(System.in);
+            System.out.println("Player 1 choose a move between rock, paper or scissor: ");
+            String player1Move = scanner1.nextLine();
+
+
             System.out.println("We will now use our toaster IQ ai pick a random move: ");
             int rand = (int) Math.random() * 3;
             String botMove = "";
-            if(rand == 0){
+            if (rand == 0) {
                 botMove = "rock";
-            }
-            else if(rand == 1){
+            } else if (rand == 1) {
                 botMove = "paper";
-            }
-            else {
+            } else {
                 botMove = "scissor";
 
             }
-            System.out.println("Player 1 have chosen "+player1Move+" as theird move");
-            System.out.println("The bot have chosen "+botMove+" as their move");
+
+            if (player1Move.equals(botMove)) {
+                System.out.println("Its a draw");
+            } else if (player1Move.equals("rock") || botMove.equals("scissor")) {
+                playerWin = true;
+                botWin = false;
+            } else if (player1Move.equals("paper") || botMove.equals("rock")) {
+                playerWin = true;
+                botWin = false;
+            } else if (player1Move.equals("scissor") || botMove.equals("paper")) {
+                playerWin = true;
+                botWin = false;
+            } else if (player1Move.equals("rocks") || botMove.equals("paper")) {
+                botWin = true;
+                playerWin = false;
+            } else if (player1Move.equals("paper") || botMove.equals("scissor")) {
+                botWin = true;
+                playerWin = false;
+            } else if (player1Move.equals("scissor") || botMove.equals("rock")) {
+                botWin = true;
+                playerWin = false;
+            }
+
+            if (playerWin == true) {
+                System.out.println("Player 1 has won");
+            } else if (botWin == true) {
+                System.out.println("The bot wins");
+            }
 
         }
-        //Mangler et compare system for hvem der vinder
+    }
 
+    //Going against weak player 2
+    public static void playPlayer(int rounds) {
+        boolean player1Win = false;
+        boolean player2Win = false;
+        int endingRound = 1;
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Player 1 tell me your name");
+        String player1Name = scanner.nextLine();
+        System.out.println("And since we have a player 2, please tell me your name");
+        String player2Name = scanner.nextLine();
+
+        for (int j = 0; j < rounds; j++) {
+
+            Scanner scanner1 = new Scanner(System.in);
+            System.out.println(player1Name + " choose a move between rock, paper or scissor: ");
+            String player1Move = scanner1.nextLine();
+
+            System.out.println("Now look away gay ass " + player1Name + " and let " + player2Name + " pick now: ");
+            String player2Move = scanner1.nextLine();
+
+            if (player1Move.equals(player2Move)) {
+                System.out.println("Its a draw");
+            } else if (player1Move.equals("rock") || player2Move.equals("scissor")) {
+                player1Win = true;
+                player2Win = false;
+            } else if (player1Move.equals("paper") || player2Move.equals("rock")) {
+                player1Win = true;
+                player2Win = false;
+            } else if (player1Move.equals("scissor") || player2Move.equals("paper")) {
+                player1Win = true;
+                player2Win = false;
+            } else if (player1Move.equals("rocks") || player2Move.equals("paper")) {
+                player2Win = true;
+                player1Win = false;
+            } else if (player1Move.equals("paper") || player2Move.equals("scissor")) {
+                player2Win = true;
+                player1Win = false;
+            } else if (player1Move.equals("scissor") || player2Move.equals("rock")) {
+                player2Win = true;
+                player1Win = false;
+            }
+            if (player1Win == true) {
+                System.out.println(player1Name + " wins round " + endingRound);
+            } else if (player2Win == true) {
+            }
+            System.out.println(player2Name + " wins round " + endingRound);
+        }
+        endingRound += 1;
     }
 }
